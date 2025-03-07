7693ca73ac04891b96360697a029d6663dd0b7a6
Pooja Ramdas <pooja.ramdas05@gmail.com>
2025-03-07 23:55:10 +0530

    Added Cylinder.java

---
+++ Cylinder.java
@@ -0,0 +1,17 @@
+class Cylinder extends Shape {
+    public Cylinder(double radius, double height) {
+        super(radius, height, 0, 0);
+    }
+
+    public double calculateArea() {
+        return 2 * Math.PI * dim_one * (dim_one + dim_two);
+    }
+
+    public double calculatePerimeter() {
+        return 0; // Cylinders do not have a perimeter
+    }
+
+    public double calculateVolume() {
+        return Math.PI * dim_one * dim_one * dim_two;
+    }
+}
